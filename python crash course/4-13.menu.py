a8537d60c40acf51f31e46abd35becc867f06b7f
Nicklas Karlsson <nicklas.carlson@protonmail.com>
2026-02-07 18:04:01 +0100

    Fixes string

--- python crash course/4-13.menu.py
+++ python crash course/4-13.menu.py
@@ -1,11 +1,12 @@
-foods = (Pizza, Burgers, Noodles, Lasagna, Pancakes);
+#foods = ("Pizza", "Burgers", "Noodles", "Lasagna", "Pancakes")
+#print(foods[0])
 
-foods = (Pizza, Burgers, Noodles, Lasagna, Pancakes)
+foods = ("Pizza", "Burgers", "Noodles", "Lasagna", "Pancakes")
 print("Starter foods:")
 for food in foods:
-    print(foods)
+    print(food)
 
-foods = (Burgers, Pasta, Fries)
+foods = ("Burgers", "Pasta", "Fries")
 print("\nUpdated foods:")
 for food in foods:
     print(food)

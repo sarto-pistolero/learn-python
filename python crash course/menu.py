bf74d0c14ac8292b067e28baf4dff31025c63a91
Nicklas Karlsson <nicklas.carlson@protonmail.com>
2026-02-07 13:46:21 +0100

    Touple lists

---
+++ python crash course/menu.py
@@ -0,0 +1,9 @@
+foods = (Pizza, Burgers, Noodles, Lasagna, Pancakes)
+print("Starter foods:")
+for food in foods:
+    print(foods)
+
+foods = (Burgers, Pasta, Fries)
+print("\nUpdated foods:")
+for food in foods:
+    print(food)

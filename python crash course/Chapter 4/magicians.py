568a10ed108122ebb8d7b8bed24bdae9292ed94b
Nicklas Karlsson <nicklas.carlson@protonmail.com>
2026-02-09 21:08:05 +0100

    expanded further on the loop, print

--- python crash course/Chapter 4/magicians.py
+++ python crash course/Chapter 4/magicians.py
@@ -1,3 +1,6 @@
 magicians =['alice', 'dave', 'carl']
 for magician in magicians:
-    print(magicians)
+    print(f"{magician.title()}), that was a great trick.")
+    print(f"I can't wait to see your next trick, {magician.title()}.\r")
+
+print("Thank you, everyone. That was a great magic show.")    
